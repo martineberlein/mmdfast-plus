0fb18858dc9390cb1b00f37d538c13e746967fd3
Martin Eberlein <33397273+martineberlein@users.noreply.github.com>
2025-09-02 16:32:29 +0200

    Add tests for Python MMD functionality and adjust data preparation sizes

--- tests/test_mmd.py
+++ tests/test_mmd.py
@@ -1,5 +1,6 @@
 import unittest
 import pandas as pd
+from pandas import DataFrame
 
 from mmd.diagnoser import discover
 from model.ml import RandomForestModel
@@ -22,7 +23,7 @@ class TestMMD(unittest.TestCase):
         print("Column types:", column_types)
 
         model = RandomForestModel()
-        model.split_and_prepare_data(df_path, test_size=0.2)
+        model.split_and_prepare_data(df_path, test_size=0.8)
         model.train()
         model.evaluate()
 
@@ -49,7 +50,7 @@ class TestMMD(unittest.TestCase):
         print("Column types:", column_types)
 
         model = RandomForestModel()
-        model.split_and_prepare_data(df_path, test_size=0.2)
+        model.split_and_prepare_data(df_path, test_size=0.5)
         model.train()
         model.evaluate()
 
@@ -73,6 +74,49 @@ class TestMMD(unittest.TestCase):
 
         result_ = result.dataframe()
         print(result_[['ruleset', 'precision', 'recall']])
+
+        for r in result_["ruleset"]:
+            print(r)
+
+    def test_python_mmd(self):
+        df_path = RESOURCES_PATH / "data_Python.csv"
+        df_path_label = RESOURCES_PATH / "label_Python.csv"
+        df_data = pd.read_csv(df_path)
+        df_label = pd.read_csv(df_path_label)
+
+        # Classify columns
+        column_types = classify_columns(df_data)
+        print("Column types:", column_types)
+
+        df = pd.concat([df_data, df_label], axis=1)
+        model = RandomForestModel(pred_label="is_conflict")
+        model.split_and_prepare_data(df, test_size=0.8)
+        model.train()
+        model.evaluate()
+
+        reducer_rf = Reducer(top_n=3, random_state=42)
+        rf_wrong_reduced = reducer_rf.fit_transform(model.X_test, model.y_test)
+        influential_features = reducer_rf.selected_features
+        print("Selected features:", influential_features)
+
+        df_reduced = model.X_test.loc[:, influential_features]
+        column_types_reduced = {i: j for i, j in column_types.items() if i in influential_features}
+        mispredictions = model.get_mispredicted_dataframe()
+
+        df_mis = pd.concat([df_reduced, mispredictions], axis=1)
+        result = discover(
+            df_mis,
+            ("misprediction", True),
+            relevant_attributes=column_types_reduced,
+            coverage=0.8,
+            allow_disjunctions=True,
+        )
+
+        result_: DataFrame = result.dataframe()
+        print(result_[['ruleset', 'precision', 'recall']])
+
+        for r in result_["ruleset"]:
+            print(r)
 
 
 if __name__ == "__main__":
